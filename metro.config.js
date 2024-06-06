871febdbbdc0ebf94315a024896c55c6377ab9c0
nirgluzman <nir.gluzman@gmail.com>
2024-06-06 16:25:34 +0200

    add Firebase JS SDK

---
+++ metro.config.js
@@ -0,0 +1,7 @@
+// https://docs.expo.dev/guides/using-firebase/#configure-metro
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.resolver.sourceExts.push('cjs');
+
+module.exports = defaultConfig;
