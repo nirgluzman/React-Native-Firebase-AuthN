04268ada66006c4c8fb672a706c3688e0fec0233
nirgluzman <nir.gluzman@gmail.com>
2024-06-07 01:00:31 +0200

    add AsyncStorage to store the Firebase auth state

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,5 +18,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+// Initialize Firebase Authentication (with AsyncStorage) and get a reference to the service
+// https://firebase.google.com/support/release-notes/js#version_1030_-_august_22_2023
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
